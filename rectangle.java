a3fc48e87a32897bdb34bd017c7873984eae2318
Patrick Matherly <patrickmatherly1994@gmail.com>
2014-11-24 19:49:45 -0500

    Create rectangle.java

---
+++ rectangle.java
@@ -0,0 +1,24 @@
+package ProblemE;
+
+public class rectangle {
+	private int x1, y1, x2, y2;
+
+	public int getx1(){
+		return x1;
+	}
+	public int gety1(){
+		return y1;
+	}
+	public int getx2(){
+		return x2;
+	}
+	public int gety2(){
+		return y2;
+	}
+	public void setPoints( int x1, int y1, int x2, int y2){
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+	}
+}
